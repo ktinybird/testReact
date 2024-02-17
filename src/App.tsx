6444e1ce1d6f50e3cdbc6a0ca58dc46a4ce19588
김재호_ktinybird <ktinybird@gmail.com>
2024-02-18 02:48:38 +0900

    React&Typescript new setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,76 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a: number = 1;
+  const b = a;
+  console.log("b>>", b);
+
+  type Camry = {
+      a?: string;
+      b?: boolean;
+      c?: number;
+  };
+
+  type TestDto = {
+      car?: [Camry];
+  };
+
+  // type TestDto1 = {
+  //     car?: {
+  //         a?: string;
+  //         b?: boolean;
+  //         c?: number;
+  //     };
+  // };
+
+  const testFn = (): void => {
+      const camry: Camry = {
+          a: "test",
+          b: true,
+          c: 1
+      };
+      const testDto: TestDto = {
+          car: [camry]
+      };
+
+      testDto.car?.push({ ...camry, c: 2 });
+      testDto.car?.push({ ...camry, c: 2 });
+      testDto.car?.push({ ...camry, c: 1 });
+      testDto.car?.push({ ...camry, c: 2 });
+      testDto.car?.push({ ...camry, c: 1 });
+
+      const { car: value } = testDto;
+
+      const extractDto = {
+          value
+      };
+      console.log("■■■", extractDto.value);
+
+      // const a: number[] = [0];
+      //  const b : object = {...a};
+      //  console.log("###", a[0]);
+
+      // let filterDto = extractDto.value?.filter((item) => item.c === 1);
+
+      // extractDto.value?.map(
+      //     (item: Camry, index: number, arr: Array<Camry>) => {
+      //         //console.log("item>>{}##{}", item.c, arr);
+      //     }
+      // );
+
+      //console.log("■■■", filterDto);
+
+      type AllowedKeys = "name" | "age";
+
+      // interface 대신 여기에 type을 사용하세요.
+      type Person = Record<AllowedKeys, unknown>;
+
+      const Human: Person = {
+          name: "Steve",
+          age: 42
+      };
+      console.log("@Human@", Human);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +88,7 @@ function App() {
         >
           Learn React
         </a>
+        <input type="button" onClick={testFn} value="클릭"></input>
       </header>
     </div>
   );
